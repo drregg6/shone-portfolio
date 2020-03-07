08c5ec1e5c68be818f3e6018f04e573652403683
Dave Regg <drregg6@gmail.com>
2020-03-07 14:47:51 -0500

    Fingers crossed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const connectDB = require('./config/db')
 
 // Config Parser
@@ -15,6 +16,15 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT =  process.env.PORT || 8080;
+
+// Deployment
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Listening on Port: ${PORT}`);
