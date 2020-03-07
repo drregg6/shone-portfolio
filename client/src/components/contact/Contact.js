01432bc7680dfa501e33b3a2fc63a6aa40f005fc
Dave Regg <drregg6@gmail.com>
2020-03-07 14:16:04 -0500

    Contact page complete

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,16 +1,72 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const Contact = props => {
+import { connect } from 'react-redux';
+import { fetchResume } from '../../actions/resume';
+import formatPhone from '../../utils/formatPhone';
+
+const Contact = ({
+  fetchResume,
+  resume: { loading, resume }
+}) => {
+  useEffect(() => {
+    fetchResume();
+  }, [loading]);
+  let address, phone;
+
+  if (resume) {
+    address = resume.address;
+    phone = resume.phone;
+  }
+
+  const renderAddress = !loading && resume ? (
+    <>
+      <h2 className="italic">Address</h2>
+      <p>
+        { address.city }, { address.state }<br />
+        { address.zip }
+      </p>
+    </>
+  ) : (
+    <h1>Loading</h1>
+  )
+
+  const renderPhone = !loading && resume ? (
+    <>
+      <h2 className="italic">Telephone</h2>
+      <p>
+        { formatPhone(phone) }
+      </p>
+    </>
+  ) : (
+    <></>
+  )
+
   return (
-    <div className="contact center-content">
-      <h1>Contact</h1>
+    <div className="contact center">
+      <h1>Contact Me</h1>
+      <div className="contact-content flex">
+        <div className="contact-address">
+          { renderAddress }
+        </div>
+        <div className="contact-phone">
+          { renderPhone }
+        </div>
+      </div>
     </div>
   )
 }
 
-// Contact.propTypes = {
+Contact.propTypes = {
+  fetchResume: PropTypes.func,
+  resume: PropTypes.object
+}
 
-// }
+const mapStateToProps = state => ({
+  resume: state.resume
+});
 
-export default Contact;
+export default connect(
+  mapStateToProps,
+  { fetchResume }
+)(Contact);
