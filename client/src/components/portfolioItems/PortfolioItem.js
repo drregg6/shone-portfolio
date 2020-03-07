eb5c44c893ae35d26ec3c523cbe05c7c78b3c8b0
Dave Regg <drregg6@gmail.com>
2020-03-07 14:28:46 -0500

    Modal works

--- client/src/components/portfolioItems/PortfolioItem.js
+++ client/src/components/portfolioItems/PortfolioItem.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Modal } from 'react-bootstrap';
+
+function MyVerticallyCenteredModal(props) {
+  const image = props.item.image;
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Body>
+        <img src={image} />
+      </Modal.Body>
+    </Modal>
+  );
+}
 
 const PortfolioItem = ({
   item
 }) => {
+  const [modalShow, setModalShow] = React.useState(false);
   return (
     <>
-      <img src={item.image} />
+      <img src={item.image} onClick={() => setModalShow(true)} />
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        item={item}
+        onHide={() => setModalShow(false)}
+      />
     </>
   )
 }
 
 PortfolioItem.propTypes = {
-
+  item: PropTypes.object
 }
 
 export default PortfolioItem;
